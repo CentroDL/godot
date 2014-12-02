1d4507e4a697423df06694388d29d45b33bead89
Paul Clairmont <pcac25@gmail.com>
2014-12-02 14:29:17 -0500

    using interpolation

--- w01/d02/student/interpolation.rb
+++ w01/d02/student/interpolation.rb
@@ -1,11 +1,13 @@
 
-# first_name = 'SOME_NAME'
-# second_name = 'SOME_OTHER_NAME'
-# third_name = 'YET_ANOTHER_NAME'
-# other_likes_count = 'SOME INTEGER VALUE'
+first_name = 'Chadd Paul'
+second_name = 'Paul Chadd'
+third_name = 'Mr. Clairmont'
+other_likes_count = '25'
 
 # Directions
 
 # 1. uncomment the variable assignments above, and assign values of your own choosing.
+#done
 # 2. use string interpolation to build a string similar to "John Thomas Suhar, Stephanie Shinn and 13 others like this."
+$stdout.puts("#{first_name}, #{second_name}, #{third_name}, and #{other_likes_count} others like this.")
 # 3. print the string to the console using $stdout.puts()
