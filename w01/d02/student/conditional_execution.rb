56cedf9385a02177fe74b13a718629531b7c0b86
Loaner 2 <vanderhoop@me.com>
2014-12-02 15:58:11 -0500

    conditional_execution.rb

---
+++ w01/d02/student/conditional_execution.rb
@@ -0,0 +1,13 @@
+# dennis_is_a_quiet_fellow = true
+# dennis_is_an_ex_con = false
+#
+# if dennis_is_a_quiet_fellow || dennis_is_an_ex_con
+  # $stdout.puts("it's cool to have Dennis over while your baby is napping.")
+# end
+
+if 5 > 6
+  $stdout.puts("5 is greater than 6")
+else 5 < 6
+  $stdout.puts("5 is less than 6")
+end
+
