e4aa823727a4507cae773277125885b1d0ffe7ac
Loaner 2 <vanderhoop@me.com>
2014-12-02 14:39:17 -0500

    string_playground.rb

--- w01/d02/student/interpolation.rb
+++ w01/d02/student/interpolation.rb
@@ -2,10 +2,12 @@ first_name = 'jesse_james'
 second_name = 'peter_wentz'
 third_name = 'janet_reno'
 other_likes_count = '13'
-$stdout.puts("#{first_name}, #{second_name}, #{third_name} and #{other_likes_count} like this.")
-
+string = "#{first_name}, #{second_name}, #{third_name} and #{other_likes_count} like this."
+$stdout.puts(string)
 # Directions
 
 # 1. uncomment the variable assignments above, and assign values of your own choosing.
 # 2. use string interpolation to build a string similar to "John Thomas Suhar, Stephanie Shinn and 13 others like this."
 # 3. print the string to the console using $stdout.puts()
+
+
