c18360fe289913ef404f5aac47cb8b58f9c28d80
Loaner 2 <vanderhoop@me.com>
2014-12-02 16:57:42 -0500

    adds loop while example

---
+++ w01/d02/student/loops.rb
@@ -0,0 +1,9 @@
+num = 31592
+
+while (num > 200)
+  lyric = "#{num} bottles of beer on the wall, #{num} bottles of beer ... #{num} bottles of beer on the wall!"
+  $stdout.puts(lyric)
+  num = num - 1
+
+
+end
