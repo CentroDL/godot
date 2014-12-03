e8469a6893f88a6fa777d1c561231c1a13969354
Paul Clairmont <pcac25@gmail.com>
2014-12-02 21:04:57 -0500

    got the randnumber working LETS GO

--- w01/d02/student/ig_88a.rb
+++ w01/d02/student/ig_88a.rb
@@ -7,16 +7,27 @@ while (gun_fight_over == false)
 
   if name == "(zap)"
     x = 0
-    while (x<20)
+    while (x < 20)
       $stdout.puts("zap")
       x = x + 1
     end
     gun_fight_over = true
+
   elsif name == "han solo"
+    x = 0
     $stdout.puts("You are mine at last, Solo!")
-    while (x<20)
+    while (x < 20)
       $stdout.puts("zap")
       x = x + 1
+    end
+
+    rand_num = rand(2)
+    $stdout.puts(rand_num)
+    if rand_num == 0
+      $stdout.puts("Solo is caught! Jabba the Hutt will pay handsomely...")
+      gun_fight_over = true
+    else
+      $stdout.puts("I'll get you next time, Solo!")
     end
 
   else
