6708cf2684e999b51fb3a9d791c295148cd4d31a
brainyandbrown <you@example.com>
2014-12-03 10:21:04 -0500

    adds w01/d02 homework

--- w01/d02/student/threepio.rb
+++ w01/d02/student/threepio.rb
@@ -4,11 +4,11 @@ Obi_Wan_Kenobi = true
 $stdout.puts("Hello, I am C-3P0, human-cyborg relations.")
 #request user's name
 $stdout.puts("And your name is?")
-	while(Obi_Wan_Kenobi)
+	if name == "Obi_Wan_Kenobi"
 		$stdout.puts("Oh, marvelous! Simply marvelous! Say hello to R2-D2; he's been looking all over for you!")
 #if not:
-	if name != "Obi-Wan Kenobi"
+	else name != "Obi-Wan Kenobi"
 		$stdout.puts("It is a pleasure to meet you, #{name}. I'm terribly sorry for prying, but you don't by any chance go by the alias of Obi-Wan Kenobi, do you?")
-	end
+	
 
 end
