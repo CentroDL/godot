5c2c1e5e6815156fb394974fc7821a11361ed970
Paul Clairmont <pcac25@gmail.com>
2014-12-02 19:02:35 -0500

    tempted fate and changed part 1 a bit

--- w01/d02/student/threepio.rb
+++ w01/d02/student/threepio.rb
@@ -8,9 +8,9 @@ else
   $stdout.puts("It is a pleasure to meet you, #{name}. I'm terribly sorry for prying, but you don't by any chance go by the alias of Obi-Wan Kenobi, do you?")
   response = $stdin.gets.chomp.downcase
   $stdout.puts(response)
-  if(response.start_with?("y"))
+  if(response.start_with?("y") && (response.length <= 3))
     $stdout.puts("Oh, Marvelous! Simply Marvelous! Say hello to R2-D2; he has been looking all over for you!")
-  elsif(response.start_with?("n"))
+  elsif(response.start_with?("n") && (response.length <= 2))
     $stdout.puts("I've really enjoyed speaking with you, #{name}, but if you'll please excuse me, I have to help my friend find someone named Obi-Wan Kenobi.")
     $stdout.puts("Well R2, I suppose we'll just have to keep looking. R2-D2: (Agreeable droid noises)")
   else
