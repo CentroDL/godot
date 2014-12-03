2cd02e9993ef5996f029205f43a4c12dcbff7443
Loaner 2 <vanderhoop@me.com>
2014-12-03 16:04:17 -0500

    adds crud practice

--- w01/d03/student/datastructures/crud_practice.rb
+++ w01/d03/student/datastructures/crud_practice.rb
@@ -1,4 +1,9 @@
 juices = ["apple", "orange", "grape"]
 juices.at(1)
 juices.unshift("lime")
-juices.pop("apple")
+juices.pop
+
+june = {"days" => 30, "weeks" => 4, "sundays" => 5}
+june["weeks"]
+june[1] = "5"
+june.delete("sundays")
