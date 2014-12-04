42b6b9cba591c0c82a5602d442c438954478e430
vanderhoop <vanderhoop@me.com>
2014-12-04 14:23:13 -0500

    adds instructor method practice

--- w01/d04/instructor/survey_taker.rb
+++ w01/d04/instructor/survey_taker.rb
@@ -1,27 +1,27 @@
 require 'pry'
 require 'colorize'
 
-# binding.pry
+binding.pry
 
-# $stdout.puts "\nHi I'm taking a survey for Faceless Corporation. I have a few quick questions.\n\n".blue
+$stdout.puts "\nHi I'm taking a survey for Faceless Corporation. I have a few quick questions.\n\n".blue
 
-# $stdout.puts "What is your name?".blue
-# name = $stdin.gets.chomp.downcase + "!"
+$stdout.puts "What is your name?".blue
+name = $stdin.gets.chomp.downcase + "!"
 
-# $stdout.puts "How old are you?".blue
-# age = $stdin.gets.chomp.downcase + "!"
+$stdout.puts "How old are you?".blue
+age = $stdin.gets.chomp.downcase + "!"
 
-# $stdout.puts "How long have you been living at this address?".blue
-# time_at_address = $stdin.gets.chomp.downcase + "!"
+$stdout.puts "How long have you been living at this address?".blue
+time_at_address = $stdin.gets.chomp.downcase + "!"
 
-# $stdout.puts "What is your marital status?".blue
-# marital_status = $stdin.gets.chomp.downcase + "!"
+$stdout.puts "What is your marital status?".blue
+marital_status = $stdin.gets.chomp.downcase + "!"
 
-# $stdout.puts "Do you like turtles?".blue
-# feelings_regarding_turtles = $stdin.gets.chomp.downcase + "!"
+$stdout.puts "Do you like turtles?".blue
+feelings_regarding_turtles = $stdin.gets.chomp.downcase + "!"
 
-# $stdout.puts "Kerry or Bush?".blue
-# kerry_or_bush = $stdin.gets.chomp.downcase + "!"
+$stdout.puts "Kerry or Bush?".blue
+kerry_or_bush = $stdin.gets.chomp.downcase + "!"
 
 
 
