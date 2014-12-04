6114e74c38fe438b0a4417f4d7bcbd46631ecb25
Loaner 2 <vanderhoop@me.com>
2014-12-04 15:27:58 -0500

    adds more examples

--- w01/d04/student/method_practice.rb
+++ w01/d04/student/method_practice.rb
@@ -1,14 +1,84 @@
-def get_answer_to(question)
-  $stdout.puts(question)
-  user_answer = $stdin.gets.chomp.downcase + "!"
-  return user_answer
+# def get_answer_to(question)
+#   $stdout.puts(question)
+#   user_answer = $stdin.gets.chomp.downcase + "!"
+#   return user_answer
+# end
+
+# brian_fav_song = get_answer_to("what's your favorite song, Brian?")
+
+# $stdout.puts("Brian's favorite song is #{brian_fav_song}.")
+
+# kristens_fav_song = get_answer_to("what's your favorite song, Kristen?")
+
+# $stdout.puts("Kristen's favorite song is #{kristens_fav_song}.")
+
+# def square (x)
+#   return (x * x)
+# end
+
+# square_of_eight = square(8)
+# $stdout.puts(square_of_eight)
+
+# def halve(some_number)
+#   return some_number / 2.0
+# end
+
+# # half_of_thirty = halve(30)
+# # $stdout.puts(half_of_thirty)
+
+# # half_of_sixty_four = halve(64)
+# # $stdout.puts(half_of_sixty_four)
+
+# halve("stringy string string")
+
+# def cube(number)
+#   return(number ** 3)
+# end
+
+# # cube_of_three = cube(3)
+# # $stdout.puts(cube_of_three)
+
+# # c
+# def calculate_area(height, width)
+#   return(height * width)
+
+# end
+
+# patricios_prison_chamber_square_footage = calculate_area(35, 40)
+# $stdout.puts(patricios_prison_chamber_square_footage)
+
+# def sing(song)
+#   $stdout.puts(song)
+#   lyric = $stdin.gets.chomp.upcase + "!!!!!!!!!"
+#   return lyric
+# end
+
+# fav_lyric = sing("What's your favorite song lyric?")
+# $stdout.puts("Your favorite song lyric is #{fav_lyric}.")
+
+# trav_fav_lyric = sing("Travis, what's your favorite song lyric?")
+# $stdout.puts("Travis' favorite song lyric is #{trav_fav_lyric}.")
+
+# def calculate_generous_tip_amount(price)
+#   return(price * 0.2)
+# end
+
+#   lunch_tip_on_dec_4 = calculate_generous_tip_amount(100)
+#   $stdout.puts("You should tip your waiter #{lunch_tip_on_dec_4}")
+
+#   dinner_tip_on_dec_4 = calculate_generous_tip_amount(150)
+#   $stdout.puts("You should tip your waiter #{dinner_tip_on_dec_4}")
+
+# def convert_kelvin_to_celcius(temp)
+#   return(temp - 273.15)
+# end
+
+#   fav_temp_in_celcius = convert_kelvin_to_celcius(50)
+#   $stdout.puts("Brrr, it's #{fav_temp_in_celcius} degrees today!!")
+
+def calculate_volume(length, width, height)
+  return(length * width * height)
 end
 
-brian_fav_song = get_answer_to("what's your favorite song, Brian?")
-
-$stdout.puts("Brian's favorite song is #{brian_fav_song}.")
-
-kristens_fav_song = get_answer_to("what's your favorite song, Kristen?")
-
-$stdout.puts("Kristen's favorite song is #{kristens_fav_song}.")
-
+  volume_of_heaven_in_cubic_feet = calculate_volume(500, 700, 200)
+  $stdout.puts("The throne room of Heaven is #{volume_of_heaven_in_cubic_feet}")
