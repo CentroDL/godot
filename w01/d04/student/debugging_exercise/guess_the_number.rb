00f1be70432543c2ea97fdd99769076c865a16be
Dennis Liaw <dennis.liaw@gmail.com>
2014-12-06 14:25:59 -0500

    cleaning git status

--- w01/d04/student/debugging_exercise/guess_the_number.rb
+++ w01/d04/student/debugging_exercise/guess_the_number.rb
@@ -4,11 +4,13 @@ puts "Guess a number between 1 and 5"
 guess = gets.chomp.to_i
 
 while secret != guess
+
   if secret <  guess
     puts "Too high! Guess again:"
   else
     puts "Too low! Guess again:"
   end
+
   guess = gets.chomp
 end
 
