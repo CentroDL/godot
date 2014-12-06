2b139e71639aeb33bac945754f6bfcd9e433d6bc
Dennis Liaw <dennis.liaw@gmail.com>
2014-12-06 15:51:52 -0500

    completes shylock.rb

--- w01/d05/student/shylock.rb
+++ w01/d05/student/shylock.rb
@@ -1,6 +1,4 @@
-# >> user input
-# :: ShylockBots emotions ::
-# Return Value
+require 'colorize'
 
 #helper method to get input
 def input
@@ -13,10 +11,10 @@ def respond(input)
  $stdout.puts( "::" + input + "::")
 end
 
-
 ducats = 10000
 ledger = {}
-
+interest = 0.35
+history = []
 #conversation loop
 loop do
 
@@ -25,31 +23,79 @@ loop do
   case command.split[0]
 
   when 'purse'
-    respond('Shylock pulls out his purse')
+    respond('ShylockBot pulls out his purse')
     $stdout.puts ducats
 
   when 'loan'
     #loan name amount ducats
     debtor = command.split[1].capitalize
-    amount = command.split[2].to_i
+    amount = command.split[2].to_f
+    time = Time.new
+
 
     if (ducats - amount) > 0
         respond("ShylockBot gives #{debtor} #{amount} ducats")
         ducats -= amount
         ledger[debtor] = amount
-        $stdout.puts("- #{amount}")
+        $stdout.puts("- #{amount}".colorize(:red))
     else
       respond("Shylock bot doesn't have enough to lend #{amount} ducats. Only #{ducats} ducats left.")
     end
+
+    log = {
+      "Date:" => "#{time.month}/#{time.day}/#{time.year}",
+      "Amount:" => "-#{amount}".colorize(:red),
+      "Debtor:" => debtor,
+      "Balance:" => ducats
+    }
+
+    history.push(log)
 
   when 'ledger'
     respond('ShylockBot pulls out his ledger')
     ledger.each { |name, debt| $stdout.puts("#{name} owes me #{debt} ducats.") }
 
+  when 'collect'
+    if command.split[1] == 'interest'
+      respond("ShylockBot collects interest at #{interest}%")
+      ledger.each { |name, debt| ledger[name] = debt*(1+interest)}
+      ledger.each { |name, debt| $stdout.puts("#{name} now owes me #{debt.round(2)} ducats.") }
+    else
+      deduction = command.split[1].to_f
+      name = command.split[4].capitalize
+      ledger[name] -= deduction
+      respond("ShylockBot opens his purse")
+      $stdout.puts("+ #{deduction}".colorize(:green))
+
+      time = Time.new
+      log = {
+      "Date:" => "#{time.month}/#{time.day}/#{time.year}",
+      "Amount:" => "+#{deduction}".colorize(:green),
+      "Debtor:" => name,
+      "Balance:" => ducats
+      }
+
+      history.push(log)
+
+    end
+
+  when 'set'
+    old_interest = interest*100
+    interest = (command.split[3].chomp('%').to_f)/100
+    respond("Shylock adjusts his interest rate from #{old_interest}% to #{interest*100}%")
+
+  when 'exit'
+    respond('Goodbye')
+    break
+
+  when 'history'
+    respond('Shylock clears his throat')
+    history.each {|hash| hash.each {|key, value| puts "#{key} #{value}"}; puts "\n"}
   else
     respond('ShylockBot looks confused')
 
   end
 
+  ledger.delete_if {|key, value| value <= 0 }
 
 end
