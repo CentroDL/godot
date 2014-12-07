f0c93e90fcae36fa83d917a3fc4ae03205e74b01
Paul Clairmont <pcac25@gmail.com>
2014-12-07 15:59:09 -0500

    adds shylock thru part 3

--- w01/d05/student/shylock_bot.rb
+++ w01/d05/student/shylock_bot.rb
@@ -11,11 +11,11 @@ end
 def check_for_loan(user_input)
   is_there_leger = user_input.split.include?("loan")
 end
+# /////////END of METHODS/////////////
 
-
+run_shylock = true
 ducat_bank = 10000
-$stdout.puts(">>")
-user_response = $stdin.gets.chomp
+ledger_record = []
 return_statement = {
   :confused => "ShylockBot looks confused",
   :purse => "ShylockBot pulls out his purse",
@@ -23,15 +23,30 @@ return_statement = {
   :loan => "ShylockBot loans money"
 }
 
-if check_for_purse(user_response)
-  $stdout.puts("#{return_statement[:purse]}")
-  $stdout.puts("#{ducat_bank} ducats")
-elsif check_for_loan(user_response)
-  response_array = user_response.split
-  $stdout.puts("::ShylockBot #{response_array[1]} #{response_array[2]} ducats::")
-end
+while run_shylock
+  $stdout.puts(">>")
+  user_response = $stdin.gets.chomp
 
-$stdout.puts("::#{return_statement[:confused]}::")
-$stdout.puts("::#{return_statement[:purse]}::")
-$stdout.puts("::#{return_statement[:ledger]}::")
+  if check_for_purse(user_response)
+    $stdout.puts("::#{return_statement[:purse]}::")
+    $stdout.puts("#{ducat_bank} ducats")
+
+  elsif check_for_loan(user_response)
+    response_array = user_response.split
+    $stdout.puts("::ShylockBot gives #{response_array[1].capitalize} #{response_array[2]} ducats::")
+    loan_amount = response_array[2].to_i
+    $stdout.puts("- #{loan_amount}")
+    ducat_bank = ducat_bank - loan_amount
+    ledger_record.push("#{response_array[1].capitalize} owes me #{loan_amount} ducats")
+
+  elsif check_for_ledger(user_response)
+    $stdout.puts("::#{return_statement[:ledger]}::")
+    $stdout.puts("#{ledger_record}")
+
+  else
+    $stdout.puts("::#{return_statement[:confused]}::")
+    $stdout.puts("Pay me back whenever")
+    run_shylock = false
+  end
+end
 
