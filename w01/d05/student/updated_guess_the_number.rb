59ecbcd41645b86b85a16f7565c32dd2cc9c3811
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-12-07 11:07:13 -0500

    fixes whitespaces

--- w01/d05/student/updated_guess_the_number.rb
+++ w01/d05/student/updated_guess_the_number.rb
@@ -1,19 +1,16 @@
 # *Read* through the following code
 # Write comments above *each line* explaining what it does
 
-# sets the GEM for this code
 require 'colorize'
 
-#set boolean hash for playing_game
 playing_game = true
 players = {}
 
-# starts the game
 while playing_game
-# if the player guesses incorrectly
+
   secret_number = (1..10).to_a.sample
   guessed = false
-# adds name to an array
+
   $stdout.print('Name? ')
   player = $stdin.gets.chomp
   players[player] = []
@@ -21,19 +18,13 @@ while playing_game
   $stdout.puts('I\'m thinking of a number between 1 and 10. Can you guess it?')
 
   while !guessed
-# while your guess is incorrect...
+
     $stdout.print('> ')
     guess = $stdin.gets.to_i
     players[player].push(guess)
-# if you guess correctly...
+
     if secret_number == guess
       guessed = true
-    elsif secret_number > guess
-      guessed = false
-      $stdout.puts("#{guess}? Too low, try again!".red)
-    elsif secret_number < guess
-      guessed = false
-      $stdout.puts("#{guess}? Too high, try again!".red)
     else
       $stdout.puts("#{guess}? Try again!".red)
     end
@@ -41,7 +32,7 @@ while playing_game
   end
 
   $stdout.puts("Great job, #{player}! You guessed in #{players[player].size} guesses.".green)
-# if user wants to play again; giving a y, Y, or yes response...
+
   $stdout.puts('New Game?')
   playing_game = $stdin.gets.chomp.downcase.start_with?('y')
 end
