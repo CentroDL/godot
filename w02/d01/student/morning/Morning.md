4e36291d1ed22072bc1efbb1e1747735f3620e58
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-12-08 08:57:03 -0500

    adds morning part 1

---
+++ w02/d01/student/morning/Morning.md
@@ -0,0 +1,23 @@
+# Good Morning!
+
+![image](http://www.spin.com/sites/all/files/tumblr_naiv4kJvgc1r02hs8o1_1280.gif)
+
+Take a look at `morning.rb`
+
+**It needs some fixing up!**
+
+Here's the example: 
+
+```
+exit unless sing("I stay out too late") == "♬I stay out too late♬"
+```
+
+The above line says 
+
+> "Exit the program `unless sing("I say out too late")` returns `"♬I stay out too late♬"`
+
+The `sing` method definition is currently empty so it doesn't return `"♬I stay out too late♬"`. Instead the program exits. 
+
+It's up to you to **write all the missing code** to make the program continue.
+
+
---
+++ w02/d01/student/morning/boneyard/exes.rb
@@ -0,0 +1,54 @@
+EXES = [
+  {
+    :name     => "Brandon Borello",
+    :inspired => ["Tim McGraw"]
+  },
+  {
+    :name     => "Sam Armstrong",
+    :inspired => ["Should've Said No"]
+  },
+  {
+    :name     => "Joe Jonas",
+    :inspired => ["Forever & Always", "Last Kiss", "Better than Revenge"]
+  },
+  {
+    :name     => "Taylor Lautner",
+    :inspired => ["Back to December"]
+  },
+  {
+    :name     => "Jake Gyllenhaal",
+    :inspired => ["We Are Never Ever Getting Back Together", "State of Grace", "All Too Well", "Girl at Home", "The Moment I Knew"]
+  },
+  {
+    :name     => "John Mayer",
+    :inspired => ["Dear John", "I Knew You Were Trouble"]
+  },
+  {
+    :name     => "Conor Kennedy",
+    :inspired => []
+  },
+  {
+    :name     => "Harry Styles",
+    :inspired => []
+  },
+  {
+    :name     => "Lucas Till",
+    :inspired => []
+  },
+  {
+    :name     => "Cory Monteith",
+    :inspired => ["Speak Now", "Mine"]
+  },
+  {
+    :name     => "Eddie Redmayne",
+    :inspired => []
+  },
+  {
+    :name     => "Zac Efron",
+    :inspired => []
+  },
+  {
+    :name     => "Mr. Nobody",
+    :inspired => []
+  }
+]
