19af907604c6519e52709d68bbb98be7aa53d1c1
vanderhoop <vanderhoop@me.com>
2014-12-09 16:13:55 -0500

    adds spotify code

--- w02/d02/instructor/spotify/main.rb
+++ w02/d02/instructor/spotify/main.rb
@@ -2,7 +2,6 @@ require_relative 'playlist'
 require_relative 'song'
 
 nothing_compares_to_you = Song.new("Nothing Compares 2 U", "Sinead O'Conner")
-nothing_compares_to_you.play
 i_know_its_over         = Song.new("I know it's over", "The Smiths")
 call_your_girlfriend    = Song.new("Call Your Girlfriend", "Robyn")
 take_it_on_the_run      = Song.new("Take It On The Run", "REO Speedwagon")
@@ -13,3 +12,5 @@ travs_new_playlist.add_song(nothing_compares_to_you)
 travs_new_playlist.add_song(i_know_its_over)
 travs_new_playlist.add_song(call_your_girlfriend)
 travs_new_playlist.add_song(take_it_on_the_run)
+
+travs_new_playlist.play
--- w02/d02/instructor/spotify/playlist.rb
+++ w02/d02/instructor/spotify/playlist.rb
@@ -11,4 +11,15 @@ class Playlist
     self.songs << song_to_add
   end
 
+  # this is an instance method for playlist objects
+  def play
+    # self.songs returns an array, which means we can call .each on it.
+    # The first time this block executes, 'song' will represent the first song in the self.songs array
+    # The second time the block executes, 'song' will represent the second song in the array/
+    self.songs.each do |song|
+      song.play
+      sleep 2
+    end
+  end
+
 end
