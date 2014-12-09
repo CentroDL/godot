f9c6d4e710861b57d706e4e6af0ca2e4f5407bd4
Paul Clairmont <pcac25@gmail.com>
2014-12-09 15:44:54 -0500

    adds shirt.rb and concert.rb classes

--- w02/d02/student/student.rb
+++ w02/d02/student/student.rb
@@ -1,5 +1,11 @@
 #Creating A Student Class
 class Student
+
+  def initialize(f_name,l_name,job)
+    @first_name = f_name
+    @last_name = l_name
+    @prior_occupation = job
+  end
 #///////////////CREATING SETTER METHODS////////////
   def set_first_name=(name)
     @first_name = name
@@ -35,13 +41,11 @@ def output_user (student)
   puts(student.get_prior_occupation)
 end
 # //////////ENDS STUDENT OUTPUT METHOD/////////////
-first_student = Student.new
-first_student.set_first_name= ("Chadd")
-first_student.set_last_name= ("Clairmont")
-first_student.set_prior_occupation= ("Scuba Dive Instructor")
+first_student = Student.new("Chadd","Clairmont","Scuba Dive Instructor")
 output_user(first_student)
 
-second_student = Student.new
+# Or do it this way with inputs
+second_student = Student.new('','','')
 second_student.set_first_name= ("Kamari")
 second_student.set_last_name= ("Aykes")
 second_student.set_prior_occupation= ("Brain Scientist")
--- w02/d02/student/user.rb
+++ w02/d02/student/user.rb
@@ -1,6 +1,13 @@
 #CREATING MY FIRST CLASS
 class User
-
+# The INITIALIZE METHOD creates a skeletal definition of an object in the user. It is also always called when a new user is created
+  def initialize(x,y,z,a)
+    @name= x
+    @email= y
+    @age= z
+    @gender= a
+    @tweets = []
+  end
 # A setter method sets an attribute (@attribute) of a User object to whatever value gets passed as an argument(x)
   def set_name=(name)
     @name = name
@@ -34,25 +41,41 @@ class User
   def get_gender
     return "Gender: " + @gender
   end
+
+  def get_tweets
+    return @tweets
+  end
 # ///////////END OF GETTER METHODS/////////////////////
 end
 
-first_user = User.new
-first_user.set_name = ("Travis")
-first_user.set_email = ("@travtavta")
-first_user.set_age = 19
-first_user.set_gender = ("female")
-puts(first_user.get_name)
-puts(first_user.get_email)
-puts(first_user.get_age)
-puts(first_user.get_gender)
+def output_user (student)
+  puts(student.get_name)
+  puts(student.get_email)
+  puts(student.get_age)
+  puts(student.get_gender)
+end
 
-second_user = User.new
-second_user.set_name = ("Dain")
-second_user.set_age = 42
-second_user.set_email = ("@gatticasnarky")
-second_user.set_gender = ("male")
-puts(second_user.get_name)
-puts(second_user.get_email)
-puts(second_user.get_age)
-puts(second_user.get_gender)
+first_user = User.new(
+  "Travis Vander Hoop",
+  "Travis@travitravtratrt",
+  19,
+  "female"
+  )
+output_user(first_user)
+# first_user.set_name = ("Travis")
+# first_user.set_email = ("@travtavta")
+# first_user.set_age = 19
+# first_user.set_gender = ("female")
+
+second_user = User.new(
+  "Brian",
+  "Briguy@bravebrian.com",
+  22,
+  "male"
+  )
+output_user(second_user)
+# second_user.set_name = ("Dain")
+# second_user.set_age = 42
+# second_user.set_email = ("@gatticasnarky")
+# second_user.set_gender = ("male")
+
