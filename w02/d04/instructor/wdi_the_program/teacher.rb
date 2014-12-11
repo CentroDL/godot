1f4b4e0131c079895936a938065dfd3965d954e2
vanderhoop <vanderhoop@me.com>
2014-12-11 10:47:48 -0500

    adds code to wdi_the_program

--- w02/d04/instructor/wdi_the_program/teacher.rb
+++ w02/d04/instructor/wdi_the_program/teacher.rb
@@ -1,3 +1,13 @@
 class Teacher < Human
 
+  def speak(x)
+    puts "#{x} (said the teacher in a soothing voice)"
+  end
+
+  def teach(y)
+    speak(y)
+    puts "(checks for understanding and redirects if necessary)"
+  end
+
 end
+
