705a09b98c3f663e2ce3910451aeffffe0ddf945
Paul Clairmont <pcac25@gmail.com>
2014-12-11 19:27:23 -0500

    adds attempt at bonus

--- w02/d04/student/back_to_the_future/back_to_the_future.rb
+++ w02/d04/student/back_to_the_future/back_to_the_future.rb
@@ -4,17 +4,23 @@ require_relative 'car'
 require_relative 'train'
 require_relative 'Skateboard'
 require_relative 'car'
+require_relative 'plane'
 
 v1 = Vehicle.new('horse and buggy', 4, 'Burton, OH')
 puts v1.description
 
 
 delorean = Car.new(2, 1981, 'DeLorean', 'DMC-12', 'gray', 'Hill Valley')
+delorean.extend(TimeTravelable)
+delorean.extend(Flyable)
+
+boeing_air_bus = Plane.new
+boeing_air_bus.extend(Flyable)
+
+my_sweet_deck = Skateboard.new("black and mean", "indo-china")
+my_sweet_deck.extend(Flyable)
+
+orient_express = Train.new('passenger train', 100, 'Istanbul')
+orient_express.extend(Flyable)
 binding.pry
-delorean.extend(Time_Travelable)
-delorean.add_passenger('Marty McFly')
-delorean.refuel             #=> true
-delorean.refuel             #=> true
-delorean.go_to('the dance') #=> "You're now at the dance."
-delorean.fuel
-binding.pry
+
