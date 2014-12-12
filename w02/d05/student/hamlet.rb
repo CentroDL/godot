1817996bee3c798f7dffad535b4fe40afb914c7a
Dennis Liaw <dennis.liaw@gmail.com>
2014-12-12 10:36:35 -0500

    adds hamlet mornex

--- w02/d05/student/hamlet.rb
+++ w02/d05/student/hamlet.rb
@@ -1,3 +1,4 @@
+require 'pry'
 hamlets = [
   "Laurence Olivier, 1948",
   "Grigori Kozintsev, 1964",
@@ -28,3 +29,52 @@ quotes_array = [
   ["What a piece of work is man! how noble in reason! how infinite in faculty! in form and moving how express and admirable! in action how like an angel! in apprehension how like a god! the beauty of the world, the paragon of animals! ", "Hamlet - (Act II, Scene II)."],
   ["When sorrows come, they come not single spies, but in battalions", "Hamlet - (Act IV, Scene V)."]
 ]
+
+hamlets.each { |hm| puts "O! the venerable #{hm.split(',')[0]} starred as Hamlet in #{hm.split(',')[1].lstrip}"}
+
+# Programatically convert the hamlets array into an array of arrays where the first element, name, is a string, and the second element, year, is an integer.
+puts 'Hamlet Array of Arrays'
+hamlets_map = hamlets.map { |h| [ h.split(',')[0], h.split(',')[1].to_i ] }
+
+puts hamlets_map
+
+hamlet_hash = hamlets_map.map { |h| {name: h[0], year: h[1]} }
+puts 'Hamlet Hash'
+puts hamlet_hash
+
+#Programatically select all the Hamlets from the 1960's and assign them to an array called swingin_hamlets
+swingin_hamlets = hamlet_hash.select { |h| h[:year].between?(1959,1970) }
+puts 'Swingin Hamlets'
+puts swingin_hamlets
+
+future_hamlets = hamlet_hash.select { |h| h[:year] >= 2000 }
+
+puts 'Future Hamlets', future_hamlets
+
+# Convert the following quotes array into an array of hashes. Each hash should have the keys :quote, :play, :act and :scene
+#["To be, or not to be: that is the question", "Hamlet - (Act III, Scene I)."]
+quotes_hash = quotes_array.map do |quote|
+  { quote: quote[0],
+    play: quote[1].split('-')[0].delete(' '),
+    act: quote[1].split('-')[1].delete('(' ')' 'Act' 'Scene' '.' ' ').split(',')[0],
+    scene: quote[1].split('-')[1].delete('(' ')' 'Act' 'Scene' '.' ' ').split(',')[1]}
+
+end
+
+
+# puts 'Quotes Hash', quotes_hash
+
+quotes_hash.each do |quote|
+  puts quote[:quote]
+  puts "#{quote[:play]} - (Act #{quote[:act]}, Scene #{quote[:scene]})."
+end
+
+
+
+
+
+
+
+
+
+
