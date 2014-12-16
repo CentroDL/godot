c00e35b0fde1f7f79072a7c973035d036ac9c97a
Dennis Liaw <dennis.liaw@gmail.com>
2014-12-16 09:58:37 -0500

    adds morning exercise for w03/d02

--- w03/d02/student/morning/spec/scrabble_word_spec.rb
+++ w03/d02/student/morning/spec/scrabble_word_spec.rb
@@ -1,5 +1,9 @@
 # What needs to be required and included in order to use the SimpleAssert library?
 # What needs to be required to use the ScrabbleWord code?
+require_relative 'simple_assert'
+require_relative '../scrabble_word'
+require 'pry'
+include SimpleAssert
 
 # Morning!
 
@@ -12,34 +16,35 @@ assert_that(
 
 # Uncomment the next assertion once the first passes!
 
-# word = ScrabbleWord.new('C')
-# assert_that(
-#   'The score should not be affected by capitalization.',
-#   word.score,
-#   3)
+word = ScrabbleWord.new('C')
+assert_that(
+  'The score should not be affected by capitalization.',
+  word.score,
+  3)
 
-# word = ScrabbleWord.new('winter')
-# assert_that(
-#   '`Scrabble#score` works with multiple letters',
-#   word.score,
-#   9)
-
-
-# word = ScrabbleWord.new('SnoWMaN')
-# assert_that(
-#   '`Scrabble#score` is not affected by capitalization',
-#   word.score,
-#   12)
+word = ScrabbleWord.new('winter')
+assert_that(
+  '`Scrabble#score` works with multiple letters',
+  word.score,
+  9)
+# binding.pry
 
 
-# word = ScrabbleWord.new('snowflake')
-# assert_that(
-#   '`ScrabbleWord#multiplier_score` multiplies a score by a factor of 2',
-#   word.multiplier_score(2),
-#   38)
+word = ScrabbleWord.new('SnoWMaN')
+assert_that(
+  '`Scrabble#score` is not affected by capitalization',
+  word.score,
+  12)
 
-# word = ScrabbleWord.new('mittens')
-# assert_that(
-#   '`ScrabbleWord#multiplier_score` can multiply scores by a factor of 3',
-#   word.multiplier_score(3),
-#   27)
+
+word = ScrabbleWord.new('snowflake')
+assert_that(
+  '`ScrabbleWord#multiplier_score` multiplies a score by a factor of 2',
+  word.multiplier_score(2),
+  38)
+
+word = ScrabbleWord.new('mittens')
+assert_that(
+  '`ScrabbleWord#multiplier_score` can multiply scores by a factor of 3',
+  word.multiplier_score(3),
+  27)
