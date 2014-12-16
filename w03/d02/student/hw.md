9fb61b85dece55f856a7809e2c41b74f4a2fd4fd
Dennis Liaw <dennis.liaw@gmail.com>
2014-12-16 18:28:00 -0500

    adds hw.md

---
+++ w03/d02/student/hw.md
@@ -0,0 +1,2 @@
+centrodl.github.io
+http://validator.w3.org/check?uri=http%3A%2F%2Fcentrodl.github.io%2F&charset=%28detect+automatically%29&doctype=Inline&group=0
--- w03/d02/student/morning/scrabble_word.rb
+++ w03/d02/student/morning/scrabble_word.rb
@@ -2,16 +2,17 @@ class ScrabbleWord
 
   attr_accessor :word, :score
 
-  def initialize(str)
-    score_hash =  { a: 1, b: 3,  c: 3, d: 2, e: 1,
+    SCORE_HASH =  { a: 1, b: 3,  c: 3, d: 2, e: 1,
     f: 4, g: 2,  h: 4, i: 1, j: 8,
     k: 5, l: 1,  m: 3, n: 1, o: 1,
     p: 3, q: 10, r: 1, s: 1, t: 1,
     u: 1, v: 4,  w: 4, x: 8, y: 4,
     z: 10 }
+
+  def initialize(str)
     @word = str.downcase.split('')
     @score = 0
-    word.each { |n| @score += score_hash[n.to_sym]}
+    word.each { |n| @score += SCORE_HASH[n.to_sym]}
   end
 
   def multiplier_score(num)
