2d9da1bd52b8c6affc0c9a44813afe9af2c106f4
Philip Hughes <philip@h4w5.com>
2014-12-18 12:10:35 -0500

    next step

--- w03/d04/instructor/sinatra_games/parlour/config.ru
+++ w03/d04/instructor/sinatra_games/parlour/config.ru
@@ -3,5 +3,6 @@ require 'sinatra/base'
 require_relative 'coin'
 require_relative 'server'
 
+
 # run our server!
 run Parlour::Server
--- w03/d04/instructor/sinatra_games/parlour/server.rb
+++ w03/d04/instructor/sinatra_games/parlour/server.rb
@@ -1,7 +1,12 @@
 module Parlour
   class Server < Sinatra::Base
+
+    enable :logging
+
     get("/coin_toss") do
-        Parlour::Coin.flip
+        @result = Parlour::Coin.flip
+        logger.info(@result)
+        render(:erb, :coin_toss)
     end
   end
 end
