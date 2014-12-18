dfd39e79179938d0b1d8a76a0a03f655cafa5d18
Philip Hughes <philip@h4w5.com>
2014-12-18 12:32:28 -0500

    add conditional

--- w03/d04/instructor/sinatra_games/parlour/server.rb
+++ w03/d04/instructor/sinatra_games/parlour/server.rb
@@ -4,7 +4,15 @@ module Parlour
     enable :logging
 
     get("/coin_toss") do
+        @guess = params[:guess]
         @result = Parlour::Coin.flip
+
+        if @guess == @result
+          @message = "Now we're playin' baby!"
+        else
+          @message = "Wrong! Better luck next time, Kid!"
+        end
+
         logger.info(@result)
         render(:erb, :coin_toss)
     end
