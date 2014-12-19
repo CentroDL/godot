570179787f7a5821d7778fe7e46a1e694611e71a
Dennis Liaw <dennis.liaw@gmail.com>
2014-12-19 15:36:44 -0500

    adds pair programming bot

---
+++ w03/d05/student/pair_programming_bot/server.rb
@@ -0,0 +1,49 @@
+require 'pry'
+module PairBot
+
+  class Server < Sinatra::Base
+
+    QUESTIONS= {
+      #[text value, yes/no?, target(s),  ]
+      pass: ["Does the test pass?", true, "/refactor", "/write_code"],
+      refactor: ["Need to refactor?", true, "/do_refactor", "/new_feature"],
+      do_refactor: ["Refactor the code", false, "/"],
+      write_test: ["Write a test", false, "/pass"],
+      write_code: ["Write just enough code for the test to pass", false, "/pass" ],
+      new_feature: ["Select a new feature to implement", false, "/", "Continue" ]
+    }
+
+    get "/" do
+      status(200)
+      @yn = true
+      @text = "Do you have a test for that?"
+      @yes = "/pass"
+      @no =  "/write_test"
+      render(:erb, :index, {layout: :default_layout})
+    end
+
+    get "/:code" do
+
+      status(200)
+      @text = QUESTIONS[ params[:code].to_sym ][0]
+      @yn =   QUESTIONS[ params[:code].to_sym ][1]
+
+      if @yn #yes/no path
+        @yes = QUESTIONS[ params[:code].to_sym ][2]
+        @no = QUESTIONS[ params[:code].to_sym ][3]
+      else  #done(blue) path
+        @target = QUESTIONS[ params[:code].to_sym ][2]
+
+        if QUESTIONS[ params[:code].to_sym ].last == 'Continue'
+          @btntext = QUESTIONS[ params[:code].to_sym ][3]
+        else
+          @btntext = 'Done'
+        end
+
+      end
+      render( :erb, :index, {layout: :default_layout})
+    end
+
+  end#Server
+end#PariBot
+
