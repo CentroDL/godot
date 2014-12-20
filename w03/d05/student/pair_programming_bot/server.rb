9056199ada15be770c2bbd963ab4a5c7909907ea
Dennis Liaw <dennis.liaw@gmail.com>
2014-12-20 18:46:21 -0500

    figured out how to condense pair programming bot into one get method

--- w03/d05/student/pair_programming_bot/server.rb
+++ w03/d05/student/pair_programming_bot/server.rb
@@ -13,36 +13,55 @@ module PairBot
       new_feature: ["Select a new feature to implement", false, "/", "Continue" ]
     }
 
-    get "/" do
-      status(200)
-      @yn = true
-      @text = "Do you have a test for that?"
-      @yes = "/pass"
-      @no =  "/write_test"
+    # GET '/?answer=yes'
+    get "/?:code?" do
+      if params[:code] == nil
+        status(200)
+        @yn = true
+        @text = "Do you have a test for that?"
+        @yes = "/pass"
+        @no =  "/write_test"
+      else
+        @text = QUESTIONS[ params[:code].to_sym ][0]
+        @yn =   QUESTIONS[ params[:code].to_sym ][1]
+
+        if @yn #yes/no path
+          @yes = QUESTIONS[ params[:code].to_sym ][2]
+          @no = QUESTIONS[ params[:code].to_sym ][3]
+        else  #done(blue) path
+          @target = QUESTIONS[ params[:code].to_sym ][2]
+
+          if QUESTIONS[ params[:code].to_sym ].last == "Continue"
+            @btntext = QUESTIONS[ params[:code].to_sym ][3]
+          else
+            @btntext = "Done"
+          end
+        end
+      end
       render(:erb, :index, {layout: :default_layout})
     end
 
-    get "/:code" do
+    # GET '/yes'
+    # get "/:code" do
+    #   status(200)
+    #   @text = QUESTIONS[ params[:code].to_sym ][0]
+    #   @yn =   QUESTIONS[ params[:code].to_sym ][1]
 
-      status(200)
-      @text = QUESTIONS[ params[:code].to_sym ][0]
-      @yn =   QUESTIONS[ params[:code].to_sym ][1]
+    #   if @yn #yes/no path
+    #     @yes = QUESTIONS[ params[:code].to_sym ][2]
+    #     @no = QUESTIONS[ params[:code].to_sym ][3]
+    #   else  #done(blue) path
+    #     @target = QUESTIONS[ params[:code].to_sym ][2]
 
-      if @yn #yes/no path
-        @yes = QUESTIONS[ params[:code].to_sym ][2]
-        @no = QUESTIONS[ params[:code].to_sym ][3]
-      else  #done(blue) path
-        @target = QUESTIONS[ params[:code].to_sym ][2]
+    #     if QUESTIONS[ params[:code].to_sym ].last == "Continue"
+    #       @btntext = QUESTIONS[ params[:code].to_sym ][3]
+    #     else
+    #       @btntext = "Done"
+    #     end
 
-        if QUESTIONS[ params[:code].to_sym ].last == 'Continue'
-          @btntext = QUESTIONS[ params[:code].to_sym ][3]
-        else
-          @btntext = 'Done'
-        end
-
-      end
-      render( :erb, :index, {layout: :default_layout})
-    end
+    #   end
+    #   render( :erb, :index, {layout: :default_layout})
+    # end
 
   end#Server
 end#PariBot
