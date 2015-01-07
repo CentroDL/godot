5d2e9c5e15ccd0998af0e69410ef406a0bca6005
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-07 18:33:15 -0500

    Adds WDInstagram exercise

--- w04/d03/student/wdinstagram/server.rb
+++ w04/d03/student/wdinstagram/server.rb
@@ -20,5 +20,30 @@ module Wdinstagram
     get('/') do
       render(:erb, :index, :layout => :default)
     end
-  end
+
+    get '/entries' do
+      entry_ids = $redis.lrange "entries", 0, -1
+      @entries = entry_ids.map { |id| $redis.hgetall "entries:#{id}" }
+      render :erb, :entries, layout: :default
+    end
+
+    get '/entries/new' do
+      render :erb, :new, layout: :default
+    end
+
+    get "/entries/:id" do
+      @entry = $redis.hgetall "entries:#{params[:id]}"
+      render :erb, :entry, layout: :default
+    end
+
+    post '/entries' do
+      id = $redis.incr "entry_id"
+      $redis.hmset "entries:#{id}", "author", params["author"], "url", params["photo_url"], "date", params["date_taken"]
+      $redis.lpush "entries", id
+      redirect '/entries'
+    end
+
+
+
+  end#Server
 end
