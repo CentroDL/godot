2ac8a98ce8b2e7bb4218449301632ada722e2384
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-06 21:43:14 -0500

    Adds Cat Tinder Exercise

--- w04/d02/student/cat_tinder/server.rb
+++ w04/d02/student/cat_tinder/server.rb
@@ -14,14 +14,26 @@ module CatTinder
       $redis = Redis.new
     end
 
+    helpers CatTinder::DatabaseHelper
+
     ########################
     # Routes
     ########################
     get '/' do
-
+      render :erb, :index, layout: :default_layout
     end
 
-  end
+    get '/cats' do
+      @cats = ids
+      render :erb, :cats, layout: :default_layout
+    end
+
+    get '/cats/:id' do
+      @id = params[:id]
+      render :erb, :cat, layout: :default_layout
+    end
+
+  end#Server
 
 end
 
