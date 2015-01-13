ab3472ab31985753d76f49d7ad5567319e51c275
vanderhoop <vanderhoop@me.com>
2015-01-13 13:58:23 -0500

    adds instructor code for deleting_in_redis.rb

--- w05/d02/instructor/deleting_in_redis.rb
+++ w05/d02/instructor/deleting_in_redis.rb
@@ -12,3 +12,19 @@ $redis.rpush("colors", "purple")
 $redis.rpush("colors", "dave")
 
 binding.pry
+
+$redis.rpop("colors")
+$redis.lpop("colors")
+
+$redis.hmset("pj", "first_name", "phillip", "last_name", "hughes", "age", 34)
+
+# overwriting a value in a hash
+$redis.hset("pj", "first_name", "PJ")
+
+# deletes age field from pj
+$redis.hdel("pj", "age")
+
+# deletes multiple key-value pairs from the 'pj hash'
+$redis.hdel("pj", ["first_name", "last_name"])
+
+
