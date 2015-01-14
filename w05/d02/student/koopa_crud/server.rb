324d9df395b75a9e395149ef63c078d5aa769278
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-13 22:13:59 -0500

    stuff

---
+++ w05/d02/student/koopa_crud/server.rb
@@ -0,0 +1,16 @@
+module Koopa
+  class Server < Sinatra::Base
+
+    group :development do
+      register Sinatra::Reloader
+      $db = Redis.new
+    end
+
+
+    get '/:id' do
+      $db.incr "page_views"
+      erb :index
+    end
+
+  end
+end
