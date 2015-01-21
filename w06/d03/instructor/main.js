6de3cb744847539e3ca45b9983a0967b7b4c6b19
Philip Hughes <philip@h4w5.com>
2015-01-21 12:35:07 -0500

    add some js

--- w06/d03/instructor/main.js
+++ w06/d03/instructor/main.js
@@ -1,1 +1,19 @@
 var instructor = "PJ";
+
+console.log("Welcome, " + instructor + "!");
+
+
+// primitives
+// use === (threequals) to compare
+var aString, anInteger, aFloat, aBoolean;
+
+aString = "This is a string.";
+anInteger = 42;
+aFloat = 3.14159;
+aBoolean = true;
+
+// data structures
+var anArray, anObject;
+
+anArray = [aString, anInteger, aFloat, aBoolean];
+anObject = {manInstructor: "Travis", dog: "Kait"};
