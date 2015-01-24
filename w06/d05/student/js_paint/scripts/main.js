1fefa0abda1e8f5ef1355ca4a82b10bba0291417
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-23 20:12:44 -0500

    adds colors and painting to hw

--- w06/d05/student/js_paint/scripts/main.js
+++ w06/d05/student/js_paint/scripts/main.js
@@ -1,23 +1,31 @@
+var color = "green";
+var colors = ["red", "goldenrod", "blue"];
+
 var mouseDepressed = false;
 
 var body = document.querySelector("body");
 
 var canvas = body.querySelector(".canvas");
+var palette = body.querySelector(".color-palette");
 
 var hoverListener = function(event){
 
-  event.target.className += " green";
+  event.target.className += " " + color;
 }
 
 var reset = function(event){
   // Awesome. Now head back to the event listener you added for mouseouts (part 2). Only remove the class green if mouseDepressed is false.
   if( !mouseDepressed )
-  event.target.classList.remove("green");
+  event.target.classList.remove(color);
 }
 
 var mouseDep = function(event){
   mouseDepressed = !mouseDepressed;
   // console.log("poop!");
+}
+
+var setColor = function(){
+  color = event.target.classList[0];
 }
 
 var clearGreen = function(event){
@@ -27,6 +35,11 @@ var clearGreen = function(event){
   }
 
 }
+for(var i=0; i<colors.length; i++){
+  var div = document.createElement("div");
+  div.className = colors[i] + " droplet";
+  palette.appendChild(div);
+}
 
 for(var i = 0; i < 805; i++){
   var tile = document.createElement("div")
@@ -34,6 +47,11 @@ for(var i = 0; i < 805; i++){
   tile.addEventListener( "mouseover" , hoverListener);
   tile.addEventListener("mouseout", reset);
   canvas.appendChild(tile);
+}
+
+var droplets = palette.querySelectorAll(".droplet");
+for(var i=0; i < droplets.length; i++){
+  droplets[i].addEventListener("click", setColor);
 }
 
 var tiles = canvas.querySelectorAll(".tile");
