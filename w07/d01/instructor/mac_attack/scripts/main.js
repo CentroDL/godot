1155baba6aea8f48fc7f48964944d37861829ea2
Philip Hughes <philip@h4w5.com>
2015-01-26 13:09:34 -0500

    onload issues

--- w07/d01/instructor/mac_attack/scripts/main.js
+++ w07/d01/instructor/mac_attack/scripts/main.js
@@ -6,10 +6,20 @@ var ul = document.createElement("ul");
 p.textContent = "A list of their albums:"
 
 window.addEventListener("load", function(){
+  console.log("LOAD");
   // add content to the DOM
   var elAlbums = document.querySelector(".albums");
   elAlbums.appendChild(p);
   elAlbums.appendChild(ul);
-});
 
+  for (var i=0, len=fleetwoodMacAlbums.length; i<len; i++) {
+    var li = document.createElement("li");
+    li.setAttribute("class", "album");
+    li.textContent = fleetwoodMacAlbums[i].title + ", " + fleetwoodMacAlbums[i].releasedOn;
+    ul.appendChild(li);
+  }
+});
+console.log("in main js, but after the load event listener");
 console.log(fleetwoodMacAlbums);
+
+
