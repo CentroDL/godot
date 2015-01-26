ce681d0220ad77621eaa0dc9afce88d8f4516882
Philip Hughes <philip@h4w5.com>
2015-01-26 11:47:32 -0500

    fixed the load issues

--- w07/d01/instructor/mac_attack/scripts/main.js
+++ w07/d01/instructor/mac_attack/scripts/main.js
@@ -5,7 +5,11 @@ var p  = document.createElement("p");
 var ul = document.createElement("ul");
 p.textContent = "A list of their albums:"
 
-// add content to the DOM
-var elAlbums = document.querySelector(".albums");
-elAlbums.appendChild(p);
-elAlbums.appendChild(ul);
+window.addEventListener("load", function(){
+  // add content to the DOM
+  var elAlbums = document.querySelector(".albums");
+  elAlbums.appendChild(p);
+  elAlbums.appendChild(ul);
+});
+
+console.log(fleetwoodMacAlbums);
