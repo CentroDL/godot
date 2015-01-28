ef511bacd80b6c01e19e307e08d3798471e4e90b
vanderhoop <vanderhoop@me.com>
2015-01-28 11:53:44 -0500

    adds jquery sandbox

---
+++ w07/d03/student/jquery_sandbox/main.js
@@ -0,0 +1,5 @@
+console.log("================================");
+console.log("Welcome to Your Sandbox Console.");
+console.log("================================");
+
+console.log("Click on http://jquery.com to head to the docs");
