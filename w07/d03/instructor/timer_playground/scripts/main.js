75587b7a6df2f847afab1308a265447763599301
vanderhoop <vanderhoop@me.com>
2015-01-28 10:46:54 -0500

    adds instructor playground work with timers

--- w07/d03/instructor/timer_playground/scripts/main.js
+++ w07/d03/instructor/timer_playground/scripts/main.js
@@ -13,6 +13,23 @@ var debate = function(){
   }
 };
 
+// setTimeout takes two arguments, the first is a function which holds the code we want to execute, and the second is the delay in milliseconds. Thus, the code below will run after 5 seconds.
+setTimeout(function(){
+  console.log("Game of Thrones, Season 5. Oh yeah.");
+}, 5000);
+
+var createP = function(){
+  var newP = document.createElement("p");
+  newP.textContent = "The Beatles";
+  body.appendChild(newP);
+};
+
+setTimeout(createP, 6000);
+
+var timerOne = setInterval(debate, 1000);
+var timerTwo = setInterval(debate, 1000);
+var timerThree = setInterval(debate, 1000);
+
 
 
 
