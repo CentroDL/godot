650b831881323097415db793b2266adb3248cf6c
vanderhoop <vanderhoop@me.com>
2015-01-28 12:03:55 -0500

    removes outdated sandbox

--- w07/d03/student/jquery_sandbox/main.js
+++
@@ -1,5 +0,0 @@
-console.log("================================");
-console.log("Welcome to Your Sandbox Console.");
-console.log("================================");
-
-console.log("Click on http://jquery.com to head to the docs");
