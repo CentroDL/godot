fe49727b5e17031c272c89f91d492d60776d2399
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-28 12:28:13 -0500

    cleans up theWalker

--- w07/d03/student/the_walker/scripts/main.js
+++ w07/d03/student/the_walker/scripts/main.js
@@ -3,26 +3,46 @@ var theWalker = document.getElementById('the-walker');
 
 theWalker.style.right = "0";
 
+
 var dx = 20;
+
+var flipRight = function(){
+  dx = -20;
+  theWalker.setAttribute("class", "walk-right");
+};
+
+var flipLeft = function(){
+  dx = 20;
+  theWalker.setAttribute("class", "");
+}
+
+var walk = function(){
+  var newPos = ( parseInt(theWalker.style.right) + dx ).toString() + "px";
+  theWalker.style.right = newPos;
+};
 
 var walkLeft = setInterval( function(){
 
-  if( parseInt(theWalker.style.right) >= 1000 ){
-    dx = -20;
-    theWalker.setAttribute("class", "walk-right");
-  }
+  if( parseInt(theWalker.style.right) >= 1000 )
+    flipRight();
 
-  if( parseInt(theWalker.style.right) <= 0 ){
-    dx = 20;
-    theWalker.setAttribute("class", "");
-  }
+  if( parseInt(theWalker.style.right) <= 0 )
+    flipLeft();
 
-  var newPos = ( parseInt(theWalker.style.right) + dx ).toString() + "px";
-  // console.log(newPos);
+  walk();
 
-
-  theWalker.style.right = newPos;
 }, 65);
+
+//TODO fix this
+theWalker.addEventListener( "click", function(){
+  console.log("clicked the walker");
+  if(dx >= 0)
+    flipLeft();
+  else
+    flipRight();
+});
+
+
 
 
 
