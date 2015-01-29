96299e78faaac302ab4a4826ad0f0318dc77a296
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2015-01-29 16:44:50 -0500

    adds lotr solution

---
+++ w07/d03/instructor/solution_lotr/scripts/characters.js
@@ -0,0 +1,23 @@
+console.log("characters.js loaded");
+
+// Dramatis Personae
+var hobbits = [
+  'Frodo Baggins',
+  'Samwise \'Sam\' Gamgee',
+  'Meriadoc \'Merry\' Brandybuck',
+  'Peregrin \'Pippin\' Took'
+];
+
+var buddies = [
+  'Gandalf the Grey',
+  'Legolas',
+  'Gimli',
+  'Strider',
+  'Boromir'
+];
+
+var lands = [
+  'The Shire',
+  'Rivendell',
+  'Mordor'
+];
