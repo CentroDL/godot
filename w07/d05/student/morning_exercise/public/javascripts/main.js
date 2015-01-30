c989f3421359e6b9810ce5465fdac42e49451315
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-30 09:27:52 -0500

    gets renaming working in mornex

--- w07/d05/student/morning_exercise/public/javascripts/main.js
+++ w07/d05/student/morning_exercise/public/javascripts/main.js
@@ -6,24 +6,19 @@ $("button.way-no-way").on("click", function(){
 
 });
 
-
-// Add an event listener to the "span.name" so that when you click on it
-
-// add the class "hidden" to it
-// remove the class class "hidden" from "input.input-name"
-// set the value of "input.input-name"' to the text of "span.name"
-
 $("span.name").on( "click", function(){
   $(this).addClass("hidden");
   $("input.input-name").removeClass("hidden");
   $("input.input-name").val( $(this).text() );
 });
 
-// Add an "keypress" event listener to "input.input-name" so that when you type in a name and hit "return"
-
-// the class "hidden" is added to "input.input-name"
-// the class "hidden" is removed from "span.name"
-// the value from "input.input-name" is set to the text of "span.name"
+$("input.input-name").on("keypress", function(e){
+  if( e.which === 13 ){
+    $(this).addClass("hidden");
+    $("span.name").removeClass("hidden");
+    $("span.name").text( $(this).val() );
+  }
+});
 
 
 
