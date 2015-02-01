e3403e664b11c0096ec88b778d8eb242548062f5
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-01 09:52:45 -0500

    adds firebase object and rewrites create task

--- w07/d05/student/firebase_todo/scripts/main.js
+++ w07/d05/student/firebase_todo/scripts/main.js
@@ -5,21 +5,22 @@ var taskList = {};
 taskList["counter"] = 0;
 // $("window").on("load", function(){});
 
-// 1. Create a new task (createTask). Make sure to give the new task a unique ID! Also, have the function return that new unique ID. (Remember how we did this with Redis?)
-var createTask = function( description, complete){
-  var uniqueID = "task" + taskList.counter;
-  taskList.uniqueID = { complete: complete, description: description };
-  taskList.counter++;// $redis.incr("task:counter");
-  return uniqueID;
-};
-
-// var firebase = new Firebase();
-
-// var createTask = function(description, complete){
-//   var snapshot = firebase.push({complete: complete, description: description});
-//   var uniqueID = snapshot.key();
+// // 1. Create a new task (createTask). Make sure to give the new task a unique ID! Also, have the function return that new unique ID. (Remember how we did this with Redis?)
+// var createTask = function( description, complete){
+//   var uniqueID = "task" + taskList.counter;
+//   taskList.uniqueID = { complete: complete, description: description };
+//   taskList.counter++;// $redis.incr("task:counter");
 //   return uniqueID;
 // };
+
+// var firebase = new Firebase();
+var TodoApp = new Firebase("https://blazing-inferno-3859.firebaseio.com/");
+
+var createTask = function(description, complete){
+  var snapshot = firebase.push( {complete: complete, description: description} );
+  var uniqueID = snapshot.key();
+  return uniqueID;
+};
 
 // 2. Mark a task as complete or as incomplete (toggleComplete), given the task's unique ID.
 var toggleComplete = function(id){
