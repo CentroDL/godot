bc06c21a4bc59b6bbd6264ad17f181be5f833fa9
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-02 08:54:20 -0500

    puts what i got, up to around 5.3

--- w07/d05/student/firebase_todo/scripts/main.js
+++ w07/d05/student/firebase_todo/scripts/main.js
@@ -1,7 +1,6 @@
 console.log('main.js loaded');
 
 var taskList = {};
-
 taskList["counter"] = 0;
 
 var TodoApp = new Firebase("https://blazing-inferno-3859.firebaseio.com/");
@@ -12,28 +11,20 @@ var createTask = function(description, complete){
   return uniqueID;
 };
 
-// Rewrite toggleComplete to work with Firebase using Firebase#child to get the child reference, and Firebase#set to change its value. For now you can have it just force complete to be false.
 var toggleComplete = function(id){
   firebase.child(id).complete = !firebase.child(id).complete;
 };
 
-//Rewrite removeTask to work with Firebase using Firebase#child and Firebase#remove().
 var removeTask = function(id){
   firebase.child(id).remove(function(){ console.log("removed entry " + id + "from firebase.")});
 };
 
-// 4. List tasks (listTasks). This should log to the console!
 var listTasks = function(){
   for(var task in taskList){
     console.log(task);
   }
 };
 
-// <div class="task">
-//   <input type="checkbox">
-//   <span class="task-description">Task description here.</span>
-//   <span class="remove">X</span>
-// </div>
 var createTaskEl = function(description, id, complete){
 
   var div = $("<div>").addClass("task").attr("data-taskid", id);
@@ -49,22 +40,17 @@ var createTaskEl = function(description, id, complete){
   return div;
 };
 
- // Write a function to add a task element to the DOM (addTaskEl)! It should take all the same inputs as createTaskEl, call createTaskEl and save its result, and then add it to the "task-list" on the DOM.
 var addTaskEl = function(description, id, complete){
   var task = createTaskEl( description, id, complete);
   $(".task-list").append(task);
   return task;
 };
 
-// 4. Toggle the class "complete" on the task element (<div>). You can grab the correct element by finding the right data attribute (toggleTaskElComplete).
-// $('*[data-customerID="22"]');
 var toggleTaskElComplete = function(id){
   $('*[data-taskid="'+id.data+'"]').toggleClass("complete");
   console.log("toggled " + id.data);
 };
 
-
-// 5. Remove the element based on the task ID (removeTaskEl).
 var removeTaskEl = function(id){
   // debugger;
   $('*[data-taskid="'+id.data+'"]').remove();
@@ -81,17 +67,11 @@ var newTask = function(description, id, complete){
   return true;
 };
 
-// Now, we need to add a click event handler to the "new-task" form's submit event that:
-// stops the form's submission,
-// gets the description from the input,
-// creates the new task in the model (saving the ID for the next step),
-// and adds the new task to the DOM (calls newTask).
-// Note: make sure that this listener is added after the DOM content is loaded!
 $("window").on("load", function(){
 
-  $("#new-task").on("click", function(e){
-    e.stopPropagation();
-    var input = e.target.val();
+  $("form").on("submit", function(e){
+    e.preventDefault();
+    var input = $("#new-task").val();
     var id = createTask( input, false);
     newTask( input, id, false);
   });
