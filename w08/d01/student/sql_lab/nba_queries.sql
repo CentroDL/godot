3658ab747a800c4265c0526f03dc322433bd0cbc
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-02 20:31:57 -0500

    completes part 3 of sql lab

---
+++ w08/d01/student/sql_lab/nba_queries.sql
@@ -0,0 +1,14 @@
+-- All columns for all players from the New York Knicks (NYK).
+SELECT * FROM players;
+-- All columns for all players from the Indiana Packers (IND) who are under 26 years old.
+SELECT * FROM players WHERE team = 'IND' AND age < 26;
+-- All columns for all players, ordered from least points scored to most points scored.
+SELECT * FROM players ORDER BY points ASC;
+-- Name and Points per game (points/games), for the players with the top 20 points per game.
+SELECT name, points/games AS points_per_game FROM players ORDER BY points/games DESC LIMIT 20;
+-- The average age for all players.
+SELECT AVG(age) AS average_age FROM players;
+-- The average age for all players on the Oklahoma City Thunder (OKC).
+SELECT AVG(age) AS average_age_on_OKC_thunders FROM players WHERE team = 'OKC';
+-- The average age for all players who played more than 40 games.
+SELECT AVG(age) AS avg_age_40 FROM players WHERE games > 40;
