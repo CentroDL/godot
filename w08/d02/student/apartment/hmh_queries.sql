aff7d3555bfed90ebb20f25bf09811cf99aa835a
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-03 19:26:52 -0500

    break point waiting on wifi

--- w08/d02/student/apartment/hmh_queries.sql
+++ w08/d02/student/apartment/hmh_queries.sql
@@ -5,12 +5,25 @@ SELECT name, age, gender FROM tenants;
 -- Retrieve all info on all tenants older than 65
 SELECT * FROM tenants WHERE age > 65;
 -- Retrieve all info on all tenants in apartment with id 20
+SELECT * FROM tenants WHERE apartment_id = 20;
 -- Retrieve all info on all tenants in apartment with ids 20 or 21
+SELECT * FROM tenants WHERE apartment_id = 20 OR apartment_id = 21;
 -- Delete all tenants whose age is greater than 65
+DELETE * FROM tenants WHERE age > 65;
 -- Change all doormen from building 3 to work night shifts.
+
 -- Create one new tenant, put them in any apartment you want
+
 -- Find just the ids for all apartments for building with 2
+
 -- Find all info for apartments in building number 3 whose price is greater than $2300
+
 -- Geriatric Birthday! Update all tenants whose age is 90 to be 91
+
 -- Change all tenants ages to increase by 1
+
 -- Find all tenants who live in an apartment that costs more than $2300
+
+
+
+
