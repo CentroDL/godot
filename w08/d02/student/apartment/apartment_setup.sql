b25306b7282335986fa0346f179e39872d0e6901
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-03 19:12:07 -0500

    adds initial db schema

---
+++ w08/d02/student/apartment/apartment_setup.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS buildings CASCADE;
+DROP TABLE IF EXISTS apartments CASCADE;
+DROP TABLE IF EXISTS tenants CASCADE;
+DROP TABLE IF EXISTS doormen CASCADE;
+
+CREATE TABLE buildings(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  address VARCHAR NOT NULL,
+  num_floors INTEGER
+);
+
+CREATE TABLE apartments(
+  id SERIAL PRIMARY KEY,
+  floor INTEGER REFERENCES buildings (num_floors), -- CHECK THIS CONSTRAINT
+  name VARCHAR NOT NULL,
+  price INTEGER NOT NULL,
+  sqft INTEGER NOT NULL,
+  bedrooms INTEGER NOT NULL,
+  bathrooms INTEGER NOT NULL
+);
+
+CREATE TABLE tenants(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  age INTEGER NOT NULL,
+  gender VARCHAR(6) NOT NULL,
+  apartment_id INTEGER REFERENCES apartments (id)
+);
+
+CREATE TABLE doormen(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  experience INTEGER NOT NULL,
+  shift VARCHAR(5) NOT NULL,
+  building_id INTEGER REFERENCES buildings (id)
+);
+
+
+
