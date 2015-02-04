ba3ee13a4ae5abfec007d2ad76ff1d9a4aeb73cd
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-03 19:22:39 -0500

    adds hmh_query file

---
+++ w08/d02/student/apartment/hmh_queries.sql
@@ -0,0 +1,16 @@
+--   Retrieve all info on all tenants
+select * from tenants;
+-- Retrieve the name, age, and gender of all tenants
+SELECT name, age, gender FROM tenants;
+-- Retrieve all info on all tenants older than 65
+SELECT * FROM tenants WHERE age > 65;
+-- Retrieve all info on all tenants in apartment with id 20
+-- Retrieve all info on all tenants in apartment with ids 20 or 21
+-- Delete all tenants whose age is greater than 65
+-- Change all doormen from building 3 to work night shifts.
+-- Create one new tenant, put them in any apartment you want
+-- Find just the ids for all apartments for building with 2
+-- Find all info for apartments in building number 3 whose price is greater than $2300
+-- Geriatric Birthday! Update all tenants whose age is 90 to be 91
+-- Change all tenants ages to increase by 1
+-- Find all tenants who live in an apartment that costs more than $2300
