8dd63b86e789d54463eb7cf5b2a4a367cb76d54d
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-10 18:26:33 -0500

    Adds new cheese functionality

--- w09/d02/student/straight_curdin/app/controllers/cheeses_controller.rb
+++ w09/d02/student/straight_curdin/app/controllers/cheeses_controller.rb
@@ -7,4 +7,17 @@ class CheesesController < ApplicationController
     @cheese = Cheese.find params[:id]
   end
 
+  def new
+    @cheese = Cheese.new
+  end
+
+  def create
+    new_cheese = Cheese.create cheese_params
+    redirect_to cheese_path(new_cheese.id)
+  end
+
+  def cheese_params
+    params.require(:cheese).permit(:name, :milk_type, :image_url, :description)
+  end
+
 end
