ae4d7a9e0961f7626942b8f9e65c5fbab13b70ec
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-11 10:27:34 -0500

    adds comments? not sure

--- w09/d03/student/luhn_algorithm/lib/luhn.rb
+++ w09/d03/student/luhn_algorithm/lib/luhn.rb
@@ -1,19 +1,23 @@
 class Luhn
 
   def self.valid?(number)
-    arr = number.to_s.split('').reverse
-    arr.map! { |n| n.to_i }
 
-    mod = arr.map.with_index do |num, id|
-      if id.odd?
+    #convert number into a string so we can split it
+    arr = number.to_s.split('').reverse #reversed for iteration
+    arr.map! { |n| n.to_i } #turns them back into integers
+
+    #all odd indices get doubled
+    modded_arr = arr.map.with_index do |num, id|
+      if id.odd? #double every odd index
         num * 2
       else
         num
       end
     end
 
-    digits_ar = mod.join.split('')
-    digits_ar.map! { |num| num.to_i }
+    digits_ar = modded_arr.join.split('') #splits double digit numbers
+    digits_ar.map! { |num| num.to_i } #turns them into ints again
+
     digits = digits_ar.inject(:+)
 
     digits % 10 == 0
