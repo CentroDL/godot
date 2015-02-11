3d3aab8c138d99f84cfa087aef142b00b41ff7a9
Philip Hughes <philip@h4w5.com>
2015-02-11 10:29:10 -0500

    d03 work

---
+++ w09/d03/student/popcorn_app/app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    User.destroy(params[:id])
+    redirect_to  users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+
+  def user_params
+    params.require(:user).permit(:login, :born_on)
+  end
+end
