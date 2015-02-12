ad2415f2122769c84030f1892291f68395c774fd
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-11 22:08:49 -0500

    adds form for new band

--- w09/d03/student/band_io/app/controllers/bands_controller.rb
+++ w09/d03/student/band_io/app/controllers/bands_controller.rb
@@ -7,4 +7,19 @@ class BandsController < ApplicationController
   def show
     @band = Band.find params[:id]
   end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    band = Band.create band_params
+    redirect_to band_path(band)
+  end
+
+  #TODO: fix boolean values in band form
+  def band_params
+    params.require(:band).permit(:name, :genre, :has_expicit_lyrics)
+  end
+
 end
