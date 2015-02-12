ba9457ee99bb6306d34618680032e3b8401fbc96
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-11 22:15:53 -0500

    Adds new form and action for venues

--- w09/d03/student/band_io/app/controllers/venues_controller.rb
+++ w09/d03/student/band_io/app/controllers/venues_controller.rb
@@ -8,4 +8,16 @@ class VenuesController < ApplicationController
     @venue = Venue.find params[:id]
   end
 
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    venue = Venue.create venue_params
+    redirect_to venue_path(venue)
+  end
+
+  def venue_params
+    params.require(:venue).permit(:name, :city, :state, :is_family_friendly )
+  end
 end
