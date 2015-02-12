54ef7d1c36a0af3abc45922e3ee8260ab41d63dd
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-11 21:58:27 -0500

    Adds views to show and index all three models

--- w09/d03/student/band_io/app/controllers/bands_controller.rb
+++ w09/d03/student/band_io/app/controllers/bands_controller.rb
@@ -1,5 +1,10 @@
 class BandsController < ApplicationController
+
   def index
     @bands = Band.all
+  end
+
+  def show
+    @band = Band.find params[:id]
   end
 end
--- w09/d03/student/band_io/app/controllers/venues_controller.rb
+++ w09/d03/student/band_io/app/controllers/venues_controller.rb
@@ -1,5 +1,11 @@
 class VenuesController < ApplicationController
+
   def index
     @venues = Venue.all
   end
+
+  def show
+    @venue = Venue.find params[:id]
+  end
+
 end
