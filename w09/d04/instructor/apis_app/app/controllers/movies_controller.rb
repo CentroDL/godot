7561e5dd3513bc6e182ef072cd346f75dab34cb0
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2015-02-12 13:01:41 -0500

    adds omdb module

--- w09/d04/instructor/apis_app/app/controllers/movies_controller.rb
+++ w09/d04/instructor/apis_app/app/controllers/movies_controller.rb
@@ -3,22 +3,24 @@ class MoviesController < ApplicationController
   end
 
   def search
-    @title = params[:title]
-    url = "http://www.omdbapi.com/?s=#{@title}&r=json"
-    response = HTTParty.get(url) #...
-    @movies = JSON.parse(response.body)["Search"]
-    # handle the JSON
-    # render the results
+    # @title = params[:title]
+    # url = "http://www.omdbapi.com/?s=#{@title}&r=json"
+    # response = HTTParty.get(url) #...
+    # @movies = JSON.parse(response.body)["Search"]
+    # # handle the JSON
+    # # render the results
+    @movies = OmdbApi.search(@title)
   end
 
   def show
-    imdb_id = params[:imdb_id]
-    # construct the url
-    url = "http://www.omdbapi.com/?i=#{imdb_id}&r=json"
-    # do an httparty request
-    response = HTTParty.get(url)
-    # parse the response as json
-    @movie = JSON.parse(response.body)
-    # render the movie info
+    # imdb_id = params[:imdb_id]
+    # # construct the url
+    # url = "http://www.omdbapi.com/?i=#{imdb_id}&r=json"
+    # # do an httparty request
+    # response = HTTParty.get(url)
+    # # parse the response as json
+    # @movie = JSON.parse(response.body)
+    # # render the movie info
+    @movie = OmdbApi.info(imdb_id)
   end
 end
---
+++ w09/d04/instructor/apis_app/app/models/omdb_api.rb
@@ -0,0 +1,16 @@
+module OmdbApi
+  def self.search(title)
+    url = "http://www.omdbapi.com/?s=#{title}&r=json"
+    response = HTTParty.get(url)
+    JSON.parse(response.body)["Search"]
+  end
+
+  def self.info(imdb_id)
+    # construct the url
+    url = "http://www.omdbapi.com/?i=#{imdb_id}&r=json"
+    # do an httparty request
+    response = HTTParty.get(url)
+    # parse the response as json
+    JSON.parse(response.body)
+  end
+end
