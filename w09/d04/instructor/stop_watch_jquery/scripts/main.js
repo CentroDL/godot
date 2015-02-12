0f05f9e7fa4414b3fec8a4e4be8a4003f9093b20
vanderhoop <vanderhoop@me.com>
2015-02-12 11:04:15 -0500

    adds stop watch jquery solution

--- w09/d04/instructor/stop_watch_jquery/scripts/main.js
+++ w09/d04/instructor/stop_watch_jquery/scripts/main.js
@@ -1,1 +1,48 @@
 console.log("main.js linked");
+
+$(document).ready(function(){
+  // caches DOM nodes
+  var $startButton = $("#start");
+  var $resetButton = $("#reset");
+  var $pauseButton = $("#pause");
+  var $headline    = $("h1");
+
+  // count starts at zero.
+  var counter = 0;
+
+  // timer inactive by default
+  var timerActive = false;
+
+  // scopes the ID variable so that all callback functions can change the value.
+  var intervalID;
+
+  $startButton.on("click", function(){
+    timerActive = true;
+    $headline.text("Time Elapsed: " + counter);
+    intervalID = setInterval(function(){
+      counter++;
+      $headline.text("Time Elapsed: " + counter);
+    }, 1000);
+  });
+
+  $resetButton.on("click", function(){
+    console.log("reset was clicked");
+    clearInterval(intervalID);
+    $headline.text("Stop Watch");
+    counter = 0;
+  });
+
+  $pauseButton.on("click", function(){
+    console.log("pause clicked");
+    if (timerActive) {
+      clearInterval(intervalID);
+      timerActive = false;
+    } else {
+      intervalID = setInterval(function(){
+        counter++;
+        $headline.text("Time Elapsed: " + counter);
+      }, 1000);
+    }
+  });
+
+});
