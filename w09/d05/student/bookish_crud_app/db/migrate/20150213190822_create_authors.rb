bc26332e787a2410ccd625bcf3cc028f498cb69b
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-13 14:40:48 -0500

    adds author migration

---
+++ w09/d05/student/bookish_crud_app/db/migrate/20150213190822_create_authors.rb
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+      t.string :name, null: false
+      t.date :born_on, null: false
+      t.date :died_on
+      t.string :gender, default: 0
+      t.boolean :has_pseudonym, default: false
+    end
+  end
+end
