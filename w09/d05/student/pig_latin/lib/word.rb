eb973ded5200aa9a1a0679bcb6a3fa1b3835ac97
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-13 09:28:02 -0500

    adds mornex

--- w09/d05/student/pig_latin/lib/word.rb
+++ w09/d05/student/pig_latin/lib/word.rb
@@ -2,4 +2,22 @@ class Word
   def initialize(original_word)
     @original_word = original_word
   end
+
+  def piglatinize
+    if vowel? @original_word
+      @original_word + "way"
+    else
+      word_array = @original_word.chars
+      first_letter = word_array.shift
+      first_letter += word_array.shift until vowel?(word_array.join)
+      word_array.join + first_letter + 'ay'
+    end
+
+  end
+
+  def vowel?(word)
+    ['a','e','i','o','u'].include? word.chars.first
+  end
+
+
 end
