3d337a41c4201efe0f041561c10b7fb5dc199806
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-15 23:07:20 -0500

    adds book routes and controller

---
+++ w09/d05/student/bookish_crud_app/app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+#      Prefix Verb   URI Pattern                 Controller#Action
+#       books GET    /books(.:format)            books#index
+#             POST   /books(.:format)            books#create
+#    new_book GET    /books/new(.:format)        books#new
+#   edit_book GET    /books/:id/edit(.:format)   books#edit
+#        book GET    /books/:id(.:format)        books#show
+#             PATCH  /books/:id(.:format)        books#update
+#             PUT    /books/:id(.:format)        books#update
+#             DELETE /books/:id(.:format)        books#destroy
+
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
