e1443b0958b56b9a628f0c38cb66d3d3c331b98f
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-24 12:25:42 -0500

    adds ajax button example

--- w11/d02/student/ajax_playground/scripts/main.js
+++ w11/d02/student/ajax_playground/scripts/main.js
@@ -3,5 +3,51 @@ $(document).ready(function(){
 
   // caches reference to body element
   var $body = $("body");
+  var $randomUserBtn = $("#random-user-btn");
+  var $femaleUserBtn = $("#female-user-btn");
+  var $maleUserBtn   = $("#male-user-btn");
+
+  $randomUserBtn.on("click", function(){
+    $.ajax({
+      url: "http:api.randomuser.me",
+      type: 'get',
+      dataType: "json"
+    }).done( function(data){
+      var user = data.results[0].user;
+      var pictureURL = user.picture.medium;
+      var image = $("<img>").attr( "src", pictureURL );
+      $body.append(image);
+    });
+  })
+
+  $femaleUserBtn.on("click", function(){
+    $.ajax({
+      url: "http://api.randomuser.me",
+      type: "get",
+      data: { gender: "female" },
+      dataType: "json"
+    }).done( function(data){
+      var user = data.results[0].user;
+      var pictureURL = user.picture.medium;
+      var image = $("<img>").attr( "src", pictureURL );
+      $body.append(image);
+    });
+  });
+
+  $maleUserBtn.on("click", function(){
+
+    $.ajax({
+       url: "http://api.randomuser.me",
+      type: "get",
+      data: { gender: "male" },
+      dataType: "json"
+    }).done( function(data){
+      var user = data.results[0].user;
+      var pictureURL = user.picture.medium;
+      var image = $("<img>").attr( "src", pictureURL );
+      $body.append(image);
+    });
+  });
+
 
 });
