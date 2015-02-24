4c41d719a010a051df4f9d8e6d53ffa53df7fde9
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-24 16:08:13 -0500

    adds initial api call

--- w11/d02/student/homework_github_browser/github_api.js
+++ w11/d02/student/homework_github_browser/github_api.js
@@ -1,1 +1,22 @@
 console.log('github_api loaded');
+
+
+$(document).ready(function(){
+
+  $(".search-form").submit(function(e){
+
+    e.preventDefault();
+    var username = $(".username-field").val();
+    $.ajax({
+      url: "https://api.github.com/users/" + username,
+      type: "get",
+      dataType: "json"
+    }).done(function(data){
+      // debugger
+      $(".user-info").text(data.login);
+    });
+
+  });
+
+
+});
