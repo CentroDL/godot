dfbdc4249c6bf8d678a70d8d007ff30692667fae
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-24 15:46:47 -0500

    adds spotify api work

--- w11/d02/student/spotify.me/scripts/main.js
+++ w11/d02/student/spotify.me/scripts/main.js
@@ -3,5 +3,30 @@
 
 $(document).ready(function(){
 
+  var form = $("form");
+
+  form.submit(function(e){
+    e.preventDefault();
+    var searchTerm = $("#search-keyword").val();
+    $.ajax({
+      url: "http://ws.spotify.com/search/1/track",
+      dataType: "json",
+      type: "get",
+      data: {
+        q: searchTerm
+      }
+    }).done(function(data){
+      var html = "";
+      var songs = data.tracks;
+      var songName, href;
+      for(var i=0, len=songs.length; i < len; i++){
+          songName = songs[i].name;
+          href = songs[i].href;
+          html += "<li><a href=\""+ href + "\">"+songName+"</a></li>";
+      }
+      $("#results").html(html);
+    });
+
+  });
 });
 
