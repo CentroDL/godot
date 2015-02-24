63e9936f5a0721b9ec27a49c77885033a77172ce
vanderhoop <vanderhoop@me.com>
2015-02-24 12:19:36 -0500

    adds instructor solution to ajax playground

--- w11/d02/instructor/ajax_playground/scripts/main.js
+++ w11/d02/instructor/ajax_playground/scripts/main.js
@@ -4,28 +4,32 @@ $(document).ready(function(){
   // caches reference to body element
   var $body = $("body");
 
-   $.ajax({
-      url: "http://api.randomuser.me",
-      type: "GET",
-      dataType: "json"
-   }).done(function(data){
-    var user = data["results"][0]["user"];
-    var pictureURL = user["picture"]["medium"];
-    var image = $("<img>").attr("src", pictureURL);
-    $("body").append(image);
-   });
+  $("#random-user-btn").on("click", function(){
+     $.ajax({
+        url: "http://api.randomuser.me",
+        type: "GET",
+        dataType: "json"
+     }).done(function(data){
+      var user = data["results"][0]["user"];
+      var pictureURL = user["picture"]["medium"];
+      var image = $("<img>").attr("src", pictureURL);
+      $("body").append(image);
+     });
+  });
 
+$("#female-user-btn").on("click", function(){
+  $.ajax({
+    url: "http://api.randomuser.me",
+    type: "GET",
+    data: { gender: "female" },
+    dataType: "json"
+  }).done(function(data){
+      var user = data["results"][0]["user"];
+      var pictureURL = user["picture"]["medium"];
+      var image = $("<img>").attr("src", pictureURL);
+      $("body").append(image);
+   });
 });
 
-//  on line 7, what is $
-//  on line 7, what is .ajax
-//  on line 7, what is {
-//  on line 8, what is url ?
-//  on line 8, what is "http://api.randomuser.me"
-//  on line 8, what is type ?
-//  on line 8, what is GET ?
-//  on line 10, what is "json"  ?
-//  on line 11, what is .done  ?
-// on line 11, what is function ?
-// on line 11, what is data ?
+});
 
