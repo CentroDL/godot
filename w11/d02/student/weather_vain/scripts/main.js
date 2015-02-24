be6701fa9996c487d3dd2c421f894f7d00f47646
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-24 14:36:51 -0500

    completes weather ajax exercise

--- w11/d02/student/weather_vain/scripts/main.js
+++ w11/d02/student/weather_vain/scripts/main.js
@@ -6,4 +6,22 @@ $(document).ready(function(){
   var currentDateString = moment().format('dddd, MMMM Do');
   $date.text(currentDateString);
 
-});
+  $(".pure-form").submit( function(e){
+    e.preventDefault();
+
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather",
+      type: "get",
+      data: {     //q: e.target.children[0].value,
+              q: $(".pure-input-rounded").val(),
+              units: "imperial"
+            },
+      dataType: 'json'
+    }).done(function(data){
+      // alert( "API call searched for " +  e.target.children[0].value );
+      $("#forecast").text("In " + data.name + " it's " + data.main.temp + " degrees Farenheit");
+    });
+
+  });//on submit
+
+});//doc on ready
