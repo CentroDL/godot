6d0cf616743411314247c3b2e357c5967d56d248
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-24 22:11:13 -0500

    type checking in js is painful

--- w11/d02/student/homework_github_browser/github_api.js
+++ w11/d02/student/homework_github_browser/github_api.js
@@ -7,16 +7,36 @@ $(document).ready(function(){
 
     e.preventDefault();
     var username = $(".username-field").val();
+    var displayName;
+    var html = "";
+
     $.ajax({
       url: "https://api.github.com/users/" + username,
       type: "get",
       dataType: "json"
     }).done(function(data){
+
+      var login = data.login;
+      var name;
+      //wow this hung me up for a while
+      if (data.name === "" || typeof data.name === "undefined") {
+        name = "None";
+        console.log("undefined name");
+      }else{
+        name = data.name;
+        console.log("name defined")
+      }
+
+      html += "<h1>" + login + "</h1>";
+      html += "<h2>Name: " + name + "</h2>";
+      html += "<img src=\"" + data.avatar_url + "\">";
+      html += "<p>Number of public gists: " + data.public_gists + "</p>";
+      html += "<p>Number of public repos: " + data.public_repos + "</p>";
+      $(".user-info").html(html);
       // debugger
-      $(".user-info").text(data.login);
     });
 
-  });
+  });//submit
 
 
-});
+});//ready
