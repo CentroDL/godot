56bea316f1205ec4e7e4b6f34912526f54601a23
vanderhoop <vanderhoop@me.com>
2015-02-26 10:26:56 -0500

    adds netflix

---
+++ w11/d04/instructor/netflix_templating/scripts/main.js
@@ -0,0 +1,41 @@
+//==============================================
+// Leave Commented Out! (For an upcoming example)
+//==============================================
+// var peepShow = {
+//   title: "Peep Show",
+//   year: "2003-2012",
+//   genre: "Comedy",
+//   totalRatings: 473727,
+//   starCount: 4.0,
+//   parentalRating: "NR",
+//   imgURL: "http://cdn3.nflximg.net/webp/9673/69673.webp",
+//   description: "Thrown out by his girlfriend, slacker Jeremy moves in with his friend Mark, who has a very different -- but still dysfunctional -- view of the world.",
+//   seasonCount: 8,
+//   creator: "Sam Bain",
+//   subtitleLanguage: "English",
+//   audioLanguage: "English"
+// };
+
+var breakingBad = {
+  title: "Breaking Bad",
+  year: "2008-2012",
+  genre: "Crime Drama",
+  totalRatings: 12880172,
+  starCount: 4.5,
+  parentalRating: "TV-MA",
+  imgURL: "http://cdn4.nflximg.net/webp/7884/4177884.webp",
+  description: "A high school chemistry teacher dying of cancer teams with a former student to secure his family's future by manufacturing and selling crystal meth.",
+  seasonCount: 5,
+  creator: "Vince Gilligan",
+  subtitleLanguage: "English",
+  audioLanguage: "English"
+};
+
+$(document).ready(function(){
+  console.log("main.js connected");
+
+  // caches references to commonly needed DOM elements
+  var $header = $("header"),
+      $main   = $(".main");
+
+});
