16c1b4527e31c8662cee4175c10c77eb77cb09e8
Dennis Liaw <dennis.liaw@gmail.com>
2015-02-26 12:08:19 -0500

    adds initial tinder template work

--- w11/d04/student/tinder/scripts/main.js
+++ w11/d04/student/tinder/scripts/main.js
@@ -3,7 +3,31 @@ $(document).ready(function(){
 
   // caches reference to commonly needed DOM elements
   var $userInfo    = $(".user-info"),
-      $xButton     = $(".x");
+      $xButton     = $(".x"),
       $heartButton = $(".heart");
+
+  //name, age, image_url
+  var tinderTemplateSource = $("#tinder-template").html();
+  var generateTinderHTML   = _.template(tinderTemplateSource);
+  var tinderHTML           = generateTinderHTML({ name: "Dan", age: 62, image_url:"http://www.nndb.com/people/244/000025169/da-sized.jpg"});
+
+  $userInfo.html(tinderHTML);
+
+  $xButton.click(function(e){
+    console.log("x clicked");
+    $.ajax({
+      url: "http://api.randomuser.me/",
+      type: "get",
+      dataType: "json"
+    }).done(function(data){
+      var userInfo = {
+        name: data.results[0].user.name.first,
+        age: (Math.random()*25) + 23 ,
+        image_url: data.results[0].user.picture.medium
+      };
+      tinderHTML = generateTinderHTML(userInfo);
+      $userInfo.html(tinderHTML);
+    });
+  });
 
 });
