285916885aeb56e0afc729a8afbe99d4a90cd817
vanderhoop <vanderhoop@me.com>
2015-02-26 12:25:51 -0500

    adds

--- w11/d04/instructor/netflix_templating/scripts/main.js
+++ w11/d04/instructor/netflix_templating/scripts/main.js
@@ -1,20 +1,26 @@
-//==============================================
-// Leave Commented Out! (For an upcoming example)
-//==============================================
-// var peepShow = {
-//   title: "Peep Show",
-//   year: "2003-2012",
-//   genre: "Comedy",
-//   totalRatings: 473727,
-//   starCount: 4.0,
-//   parentalRating: "NR",
-//   imgURL: "http://cdn3.nflximg.net/webp/9673/69673.webp",
-//   description: "Thrown out by his girlfriend, slacker Jeremy moves in with his friend Mark, who has a very different -- but still dysfunctional -- view of the world.",
-//   seasonCount: 8,
-//   creator: "Sam Bain",
-//   subtitleLanguage: "English",
-//   audioLanguage: "English"
-// };
+var movies = [
+  { title: "Chef", imgURL: "http://cdn5.nflximg.net/webp/9665/11949665.webp" },
+  { title: "House of Cards", imgURL: "http://cdn7.nflximg.net/webp/7677/11747677.webp" },
+  { title: "His Girl Friday", imgURL: "http://cdn0.nflximg.net/images/0030/2210030.jpg" },
+  { title: "Chinatown", imgURL: "http://cdn0.nflximg.net/images/3158/11143158.jpg" },
+  { title: "What's Eating Gilbert Grape?", imgURL: "http://cdn9.nflximg.net/webp/1459/8191459.webp" },
+  { title: "Robocop (1987)", imgURL: "http://cdn6.nflximg.net/webp/0456/8730456.webp" }
+];
+
+var peepShow = {
+  title: "Peep Show",
+  year: "2003-2012",
+  genre: "Comedy",
+  totalRatings: 473727,
+  starCount: 4.0,
+  parentalRating: "NR",
+  imgURL: "http://cdn3.nflximg.net/webp/9673/69673.webp",
+  description: "Thrown out by his girlfriend, slacker Jeremy moves in with his friend Mark, who has a very different -- but still dysfunctional -- view of the world.",
+  seasonCount: 8,
+  creator: "Sam Bain",
+  subtitleLanguage: "English",
+  audioLanguage: "English"
+};
 
 var breakingBad = {
   title: "Breaking Bad",
@@ -38,4 +44,26 @@ $(document).ready(function(){
   var $header = $("header"),
       $main   = $(".main");
 
+    var showTemplateSource = $("#show-template").html();
+
+    // we pass the innerHTML of the template to the template function, which returns a new function
+    var generateShowHTML = _.template(showTemplateSource);
+    var breakingBadHTML = generateShowHTML(breakingBad);
+    var peepShowHTML = generateShowHTML(peepShow);
+
+    $main.html(peepShowHTML);
+
+    // grabs template source as a string
+    var userTemplateSource = $("#user-template").html();
+
+    // generates new function and saves it to variable
+    var generateUserHTML = _.template(userTemplateSource);
+    userHTML = generateUserHTML({ imgHREF: "http://www.vanderhoopconstruction.com/_Media/derek_vander_hoop_photo_med.jpeg", firstName: "Derek" });
+    $header.append(userHTML);
+
+    var listViewTemplateSource = $("#list-view-template").html();
+
+    var listViewTemplateFunction = _.template(listViewTemplateSource);
+    var moviesHTMLString = listViewTemplateFunction(movies);
+    $main.html(moviesHTMLString);
 });
