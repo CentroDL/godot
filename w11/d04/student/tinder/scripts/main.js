f23e0020f273e621c1ebf93af7acf0d07433e086
vanderhoop <vanderhoop@me.com>
2015-02-26 11:39:04 -0500

    adds tinder

---
+++ w11/d04/student/tinder/scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  console.log("main.js linked");
+
+  // caches reference to commonly needed DOM elements
+  var $userInfo    = $(".user-info"),
+      $xButton     = $(".x");
+      $heartButton = $(".heart");
+
+});
