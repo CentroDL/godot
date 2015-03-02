93b8f027dc4195f3c9f5188ce068e4dc703d955c
h4w5 <philip@h4w5.com>
2015-03-02 16:24:56 -0500

    appy app

--- w12/d01/instructor/dog.js
+++ w12/d01/instructor/dog.js
@@ -1,9 +1,8 @@
 console.log('woof! Dog "class" loaded!');
 
-var dogApp = {};
-dogApp.views = {};
+var app = {};
 
-dogApp.views.Dog = function(name, parentDiv) {
+app.Dog = function(name, parentDiv) {
   this.name = name;
   this.age  = 0;
   this.$parentDiv = parentDiv;
@@ -17,16 +16,16 @@ dogApp.views.Dog = function(name, parentDiv) {
 // lassie.age  = 0;
 
 $(document).ready(function() {
-  dogApp.views.Dog.prototype = {
+  app.Dog.prototype = {
     $template:  $('#dog-template'),
     bark: function() {return 'bark bark!';},
     sit: function() {return 'sits';},
     save: function(boysName) {return 'woof (ok, ill save ' + boysName + ')!';}
   };
 
-  dogApp.views.Dog.prototype.render = _.template(dogApp.views.Dog.prototype.$template.html())
+  app.Dog.prototype.render = _.template(app.Dog.prototype.$template.html())
 
-  dogApp.views.Dog.prototype.addToPage = function() {
+  app.Dog.prototype.addToPage = function() {
     console.log(this);
     var stringOfHtml = this.render({name: this.name, age: this.age});
     $(stringOfHtml).appendTo(this.$parentDiv);
