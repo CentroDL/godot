c2e02f35c17a5ee73f1e3c7ace307738cb740903
vanderhoop <vanderhoop@me.com>
2015-03-03 12:20:51 -0500

    updates app.html.erb

--- w12/d02/instructor/tmnt/app/assets/javascripts/main.js
+++ w12/d02/instructor/tmnt/app/assets/javascripts/main.js
@@ -1,5 +1,31 @@
 var turtleData;
 
+// returns a constructor function
+var ReporterModel = Backbone.Model.extend({
+    url: "http://localhost:3000/reporter"
+});
+
+// here, we instantiate a new ReporterModel
+var april = new ReporterModel();
+
+april.get("name")  // => undefined (nothing there)
+
+// makes GET request to specified URL and sets mirrored attributes
+april.fetch()
+
+april.get("name") // => "April O'Neil"
+
+
+
+var RatModel = Backbone.Model.extend({
+  url: "rat"
+});
+
+var splinter = new RatModel();
+
+splinter.fetch();
+
+debugger
 $(document).ready(function(){
 
 });
